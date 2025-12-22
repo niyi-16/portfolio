15950c1cb7bd98ed6f87a8f181bde3cab4e74a03
niyi-16 <dasil.adam@gmail.com>
2025-12-21 22:45:38 -0400

    Refactored file structure, implemented routing with React Router, and updated styles to use SCSS.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
 import { StrictMode} from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import {BrowserRouter as Router, Routes,  Route} from 'react-router-dom'
+
+import './main.scss'
+import App from './routes/Home/App.tsx'
+import Navbar from "../src/ui/Navbar/Navbar.tsx"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+      <Navbar/>
+      <Router>
+          <Routes>
+              <Route path="/" element={<App/>}></Route>
+              <Route path="/Projects" element={<App/>}></Route>
+          </Routes>
+      </Router>
   </StrictMode>,
 )
