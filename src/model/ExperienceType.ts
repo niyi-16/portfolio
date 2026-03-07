7c1e956b616840ff756663e649da2011d60016aa
niyi-16 <dasil.adam@gmail.com>
2026-03-07 12:34:44 -0400

    Add `ExperienceType` to define structure for experience objects

---
+++ src/model/ExperienceType.ts
@@ -0,0 +1,16 @@
+export type ExperienceType =  {
+    _id: string;
+    title: string;
+    company: string;
+    location: {
+        province: string;
+        country: string;
+    }
+    employment_type: string;
+    start_date: string; // ISO format (YYYY-MM)
+    end_date: string | null;
+    current: boolean;
+    summary: string;
+    responsibilities: string[];
+    skills: string[];
+}
