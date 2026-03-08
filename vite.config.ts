ba8ce9b9468797bed5659275be8215dbb7eef290
niyi-16 <dasil.adam@gmail.com>
2026-03-07 20:49:43 -0400

    changes for deployment - 3

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://niyi-16.github.io/portfolio/",
+  base: "https://niyi-16.github.io/",
   plugins: [react()],
   css: {
     preprocessorOptions:{
